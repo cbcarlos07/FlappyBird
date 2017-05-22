73153533fb5fc8b4080780442fb4f5a4d85368ec
cbcarlos07 <cbcarlos08@gmail.com>
2017-05-21 22:06:01 -0400

    Curso de Android

--- core/src/com/cursoandroid/flappybird/FlappyBird.java
+++ core/src/com/cursoandroid/flappybird/FlappyBird.java
@@ -7,27 +7,29 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+    ///private int contador = 0;
+    private SpriteBatch batch;
+    private Texture passaro;
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//Gdx.app.log("Create", "Inicializado o jogo");
+
+        batch = new SpriteBatch();
+        passaro =  new Texture("passaro1.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+        /*contador++;
+        Gdx.app.log("Render", "Renderizando o jogo: "+contador);*/
+
+        batch.begin();
+
+        batch.draw(passaro, 0, 0);
+
+        batch.end();
+
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
+
 }
