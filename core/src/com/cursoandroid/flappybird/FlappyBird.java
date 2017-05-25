7ebc88d42e55cf7486c5ca2b1942800add0a7d87
Carlos Bruno <cbcarlos08@gmail.com>
2017-05-25 09:03:47 -0400

    Curso de Android - Pontuação ao passar os canos

--- core/src/com/cursoandroid/flappybird/FlappyBird.java
+++ core/src/com/cursoandroid/flappybird/FlappyBird.java
@@ -2,8 +2,10 @@ package com.cursoandroid.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.util.Random;
@@ -16,12 +18,15 @@ public class FlappyBird extends ApplicationAdapter {
     private Texture canoBaixo;
     private Texture canoTopo;
     private Random numeroRandomico;
+    private BitmapFont fonte;
 
 
     //Atriguto de configuração
 
     private int larguraDispositivo;
     private int alturaDispositivo;
+    private int estadoJogo = 0; // 0 -> jogo nao iniciado; 1 -> jogo iniciado
+    private int pontuacao = 0;
 
     private float variacao = 0;
     private float velocidadeQueda = 0;
@@ -31,12 +36,19 @@ public class FlappyBird extends ApplicationAdapter {
     private float deltaTime;
     private float alturaEntreCanosRandomica;
 
+    private boolean marcouPonto;
+
 	@Override
 	public void create () {
 		//Gdx.app.log("Create", "Inicializado o jogo");
 
         batch = new SpriteBatch();
         numeroRandomico = new Random();
+        fonte = new BitmapFont();
+        fonte.setColor(Color.WHITE);
+        fonte.getData().setScale(6);
+
+
         passaros =  new Texture[3];
         passaros[0] = new Texture("passaro1.png");
         passaros[1] = new Texture("passaro2.png");
@@ -55,47 +67,78 @@ public class FlappyBird extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+
+        /** ESSA PRIMEIRA PARTE O PASSARO FICA ESPERANDO O PRIMEIRO TOQUE PARA COMEÇAR **/
         deltaTime = Gdx.graphics.getDeltaTime();
 
-        /*contador++;
-        Gdx.app.log("Render", "Renderizando o jogo: "+contador);*/
+                /*contador++;
+                Gdx.app.log("Render", "Renderizando o jogo: "+contador);*/
 
         variacao += deltaTime * 10;
-        posicaoMovimentoCanoHorizontal -= deltaTime * 200; //velocidade do movimento do cano horizontal
-        velocidadeQueda++;
 
-        Gdx.app.log("Variacao", "Variacao: "+Gdx.graphics.getDeltaTime());
+
+
 
         if( variacao > 2 ){
             variacao = 0;
         }
 
+        /**
+         * A TELA AINDA NAO FOI TOCADA
+         */
+        if( estadoJogo == 0 ){ // Não iniciado
 
-        if(Gdx.input.justTouched()){
-           //Gdx.app.log("Toque","Toque na tela");
-            velocidadeQueda = -15;
+            if( Gdx.input.justTouched() ){
+                estadoJogo = 1;
+            }
+
+        }else{
+
+                    posicaoMovimentoCanoHorizontal -= deltaTime * 200; //velocidade do movimento do cano horizontal
+                    velocidadeQueda++;
+
+                    Gdx.app.log("Variacao", "Variacao: "+Gdx.graphics.getDeltaTime());
+
+
+
+                    if(Gdx.input.justTouched()){
+                        //Gdx.app.log("Toque","Toque na tela");
+                        velocidadeQueda = -15;
+                    }
+
+
+                    if( posicaoInicialVertical > 0 || velocidadeQueda  < 0)
+                        posicaoInicialVertical -= velocidadeQueda;
+
+                    //verifica se o cano saiu internamente da tela
+                    if(posicaoMovimentoCanoHorizontal < -canoTopo.getWidth()){
+                        posicaoMovimentoCanoHorizontal = larguraDispositivo;
+                        alturaEntreCanosRandomica = numeroRandomico.nextInt(400) - 200;
+                        marcouPonto = false;
+
+                    }
+                    // verifica a pontuação
+                    if( posicaoMovimentoCanoHorizontal < 120 ){ // cano passou do passaro
+                        if( !marcouPonto){
+                            marcouPonto = true;
+                            pontuacao++;
+                        }
+
+                    }
+
         }
+            batch.begin();
+
+            //batch.draw(x, y);
+            batch.draw(fundo,0,0, larguraDispositivo, alturaDispositivo);
+            batch.draw( canoTopo, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2 + espacoEntreCanos / 2 + alturaEntreCanosRandomica);
+            batch.draw( canoBaixo, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos / 2 + alturaEntreCanosRandomica);
+            batch.draw( passaros[  (int) variacao ], 120, posicaoInicialVertical );
+            fonte.draw(batch, String.valueOf(pontuacao), larguraDispositivo / 2, alturaDispositivo - 50); //exibindo a pontuacao
+
+            batch.end();
 
 
-        if( posicaoInicialVertical > 0 || velocidadeQueda  < 0)
-            posicaoInicialVertical -= velocidadeQueda;
-
-        //verifica se o cano saiu internamente da tela
-        if(posicaoMovimentoCanoHorizontal < -canoTopo.getWidth()){
-            posicaoMovimentoCanoHorizontal = larguraDispositivo;
-            alturaEntreCanosRandomica = numeroRandomico.nextInt(400) - 200;
-
-        }
-
-        batch.begin();
-
-        //batch.draw(x, y);
-        batch.draw(fundo,0,0, larguraDispositivo, alturaDispositivo);
-        batch.draw( canoTopo, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2 + espacoEntreCanos / 2 + alturaEntreCanosRandomica);
-        batch.draw( canoBaixo, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos / 2 + alturaEntreCanosRandomica);
-        batch.draw( passaros[  (int) variacao ], 120, posicaoInicialVertical );
-
-        batch.end();
 
 	}
 
